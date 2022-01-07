fd6ca7e68fbe2d8c317c757492f37ddfe107b0f2
rohitkuk <rohit.kukreja01@gmail.com>
2022-01-08 01:35:18 +0530

    ..

--- yolox/utils/visualize.py
+++ yolox/utils/visualize.py
@@ -103,7 +103,9 @@ def vis10(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
             count +=1
         elif label != current:
             count = 0 
+
         print(emojidict[label][count].shape)
+        print(img.shape)
         img = cvzone.overlayPNG(img, emojidict[label][count] ,(int(box[0]), int(box[1])))
         current = label
         
